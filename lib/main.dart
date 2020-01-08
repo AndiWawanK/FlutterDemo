20f232a0b1c7ed3a114510ceccd7faac41ec78e3
Andi WawanK <andifirwansyah250@gmail.com>
2020-01-08 08:53:19 +0700

    infinite scrolling ListView

--- lib/main.dart
+++ lib/main.dart
@@ -3,32 +3,59 @@ import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
-class RandomWordsState extends State<RandomWords>{
+class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return Divider();
+
+        final index = i ~/ 2;
+
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+
+        return _buildRow(_suggestions[index]);
+      });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+
   @override
-  Widget build(BuildContext context){
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Mantan Name Generator"),
+      ),
+      body: _buildSuggestions(),
+    );
+    // final wordPair = WordPair.random();
+    // return Text(wordPair.asPascalCase);
   }
 }
 
-class RandomWords extends StatefulWidget{
+class RandomWords extends StatefulWidget {
   @override
   RandomWordsState createState() => RandomWordsState();
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Welcome to Flutter"),
-        ),
-        body: Center(
-          child: RandomWords(),
-        ),
-      ),
+      title: 'Mantan Name Generator',
+      home: RandomWords(),
     );
   }
 }
